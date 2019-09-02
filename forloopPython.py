ed9aefa5abf4f63b934278f08395f915ff9b5f8e
CJHydraGenZ <cahyasaga321@gmail.com>
2019-09-03 01:16:50 +0800

    update script

---
+++ forloopPython.py
@@ -0,0 +1,27 @@
+
+# nama = ['cahya', 'dodi', 'jodi', 'memek']
+
+# for a in nama:
+#     print(a)
+#     print(len(a))
+
+
+# itereble
+
+# cahya = 'cahya'
+
+# for i in cahya:
+#     print(i)
+
+
+# for di dalam for
+nama = ['cahya', 'dodi', 'jodi', 'memek']
+buah = ['salak', 'duren', 'mangga', 'apel']
+sayue = ['kangkung', 'wortel', 'buncis', 'kentang']
+
+
+daftar_belanja = [nama, buah, sayue]
+
+# print(daftar_belanja)
+for subDatrBelanja in daftar_belanja:
+    print(subDatrBelanja)
