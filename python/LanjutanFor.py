1693de5915d1bc800529218d08192e488c40b811
CJHydraGenZ <cahyasaga321@gmail.com>
2020-02-07 23:25:10 +0800

    cm

---
+++ python/LanjutanFor.py
@@ -0,0 +1,19 @@
+
+# number = 25
+
+
+# for i in range(0, 50):
+#     print(i)
+
+#     if i is number:
+#         print('angka di temukan', i)
+#         break
+# else:
+#     print('angka tidak ditemukan')
+
+
+bintang = '*'
+
+for a in range(0, 10):
+    for b in range(0, 10):
+        print(bintang, '')
---
+++ python/ctypes.py
@@ -0,0 +1,4 @@
+
+
+from ctypes import *
+from ctypes.wintypes import *
